f237aa9a7cb65cb6b9e68a0e5d49679e5ca40f35
jair <jairdias05@gmail.com>
2022-11-03 09:47:24 -0300

    Main.js refatorado
    Foi feita uma refatoração para dar mais dinamismo para o main.js.

--- main.js
+++ main.js
@@ -24,14 +24,14 @@ document.querySelector("#cards").innerHTML =
 
 
 
-    ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
-     ${createCard(
+    createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))+
+     createCard(
        "28/11",
        "segunda",
        createGame("brazil", "13:00", "switzerland") +
          createGame("portugal", "16:00", "uruguay")
-     )}
+     )+
 
-     ${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))}
+     createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
  
  
